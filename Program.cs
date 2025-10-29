dde96ba561619ec6da4aaf9d88a8b4cbecd207f9
kenti <016mki69@gmail.com>
2025-10-29 15:24:49 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Raven.Client.Documents;
+
+class Program
+{
+    static void Main()
+    {
+        var store = new DocumentStore
+        {
+            Urls = new[] { "http://localhost:8080" },
+            Database = "testing"
+        }.Initialize();
+
+        using (var session = store.OpenSession())
+        {
+            var user = new User{
+            Name= "pepe",
+            City= "CABA",
+            Age= 28
+            };
+            session.Store(user);
+            session.SaveChanges();
+        }
+
+        Console.WriteLine("Documento guardado!");
+    }
+}
